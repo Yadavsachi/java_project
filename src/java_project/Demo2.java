bd8757e82214fc13b9b7be622c1fc581339357dc
sonaliyadav13856@gmail.com <sonaliyadav13856@gmail.com>
2022-12-21 00:49:53 +0530

    new code

---
+++ src/java_project/Demo2.java
@@ -0,0 +1,10 @@
+package java_project;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome softech family");
+	}
+
+}
